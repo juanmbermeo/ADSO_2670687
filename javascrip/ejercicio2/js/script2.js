cd359b5692602c8a4ef1000dcee977bb794da982
juanmbermeo <bermeojuanmanuel1@gmail.com>
2023-07-26 15:22:09 -0500

    se realizo el primer ejercicio

--- javascrip/ejercicio2/js/script2.js
+++ javascrip/ejercicio2/js/script2.js
@@ -6,8 +6,7 @@ const nsueldo = sueldo + parseInt(sueldo*psueldo);
 alert("el nuevo sueldo con el aumento es: "+nsueldo);*/
 
 /* Ejercicio 3 - un vendedor que tiene su sueldo base, recibe una comision de 30% del total de las ventas de un mes, el quiere saber cuanto ganara en un mes que tuvo tres ventas*/
-
-let sueldo = prompt("ingrese el valor del sueldo");
+/*let sueldo = prompt("ingrese el valor del sueldo");
 let venta1 = prompt("ingrese el valor de la venta");
 let venta2 = prompt("ingrese el valor de la venta");
 let venta3 = prompt("ingrese el valor de la venta");
@@ -16,4 +15,13 @@ let comision = parseInt(venta1)+ parseInt(venta2)+ parseInt(venta3);
 let resul = parseInt(sueldo) + parseInt(comision*porc);
 let resulf = parseInt(resul);
 
-alert("su sueldo jua a la comision por las tres ventass es de $" +resulf* " COP");
+alert("su sueldo junto a la comision por las tres ventass es de $" + resulf+ " COP");*/
+
+/* Ejercio 3 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra*/
+
+let compra = prompt("ingrse el valor de la compra");
+let desc = 25/100;
+let res = parseInt(compra)- parseInt(compra*desc);
+
+alert("el valor total a pagar esde $ "+res);
