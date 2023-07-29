bc2521a91c8ab0347ec8b407e988474fca605b93
Juanmbermeo <bermeojuanmanuel1@gmail.com>
2023-07-28 20:37:30 -0500

    se realizo el noveno ejercicio

--- javascrip/ejercicio2/js/script2.js
+++ javascrip/ejercicio2/js/script2.js
@@ -5,7 +5,7 @@ const psueldo= 10/100;
 const nsueldo = sueldo + parseInt(sueldo*psueldo);
 alert("el nuevo sueldo con el aumento es: "+nsueldo);*/
 
-/* Ejercicio 3 - un vendedor que tiene su sueldo base, recibe una comision de 30% del total de las ventas de un mes, el quiere saber cuanto ganara en un mes que tuvo tres ventas*/
+/*un vendedor que tiene su sueldo base, recibe una comision de 30% del total de las ventas de un mes, el quiere saber cuanto ganara en un mes que tuvo tres ventas*/
 /*let sueldo = prompt("ingrese el valor del sueldo");
 let venta1 = prompt("ingrese el valor de la venta");
 let venta2 = prompt("ingrese el valor de la venta");
@@ -17,7 +17,7 @@ let resulf = parseInt(resul);
 
 alert("su sueldo junto a la comision por las tres ventass es de $" + resulf+ " COP");*/
 
-/* Ejercio 3 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+/* Ejercio 1 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
 saber cuánto deberá pagar por su compra*/
 
 let compra = prompt("ingrse el valor de la compra");
@@ -26,7 +26,7 @@ let res = parseInt(compra)- parseInt(compra*desc);
 
 alert("el valor total a pagar esde $ "+res);
 
-/* Ejercicio 4 - Suponga que un individuo desea invertir su capital en un banco y desea saber
+/* Ejercicio 2 - Suponga que un individuo desea invertir su capital en un banco y desea saber
 cuánto dinero ganara después de un mes si el banco paga a razón de 2%
 mensual */
 
@@ -36,7 +36,7 @@ let resu = parseInt(dinero)+ parseInt(dinero*raz);
 
 alert("el dinero que usted a ganado en el transcurso de este mes es $ " +resu);
 
-/*Ejercicio 5 - Un alumno desea saber cuál será su calificación final en la materia de
+/*Ejercicio 3 - Un alumno desea saber cuál será su calificación final en la materia de
 Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
 a. 55% del promedio de sus tres calificaciones parciales.
 b. 30% de la calificación del examen final.
@@ -55,7 +55,7 @@ let cf = parseInt(pp)+ parseInt(pe)+ parseInt(pt);
 
 alert("su calificacion final en la materia de algoritmos es de " +cf);
 
-/* Ejercicio 6 - El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+/* Ejercicio 4 - El dueño de una tienda compra un artículo a un precio determinado. Obtener el
 precio en que lo debe vender para obtener una ganancia del 30%*/
 
 let pa = prompt("Ingresa el valor del articulo comprado");
@@ -64,7 +64,7 @@ let ptotal = parseInt(pa)+ parseInt(pa*porce);
 
 alert("el precio al que debe vender el producto es de $ " +ptotal);
 
-/* Ejercicio 7 - Un taxi cobra por una carrera 15 mil pesos por kilómetro recorrido y 2 mil por
+/* Ejercicio 5 - Un taxi cobra por una carrera 15 mil pesos por kilómetro recorrido y 2 mil por
 minuto. Determine el monto a pagar por una carrera. */
 
 let distancia = prompt("ingrese la distancia recorrida en kilometros");
@@ -73,7 +73,7 @@ let mpagar = parseInt(15000*distancia)+ parseInt(2000*tiempo);
 
 alert("el monto a pagar es de $ " + mpagar);
 
-/*Realizara un programa en el cual se van a leer tres números y determinar el
+/*Ejercicio 6 - Realizara un programa en el cual se van a leer tres números y determinar el
 cuadrado de cada uno de ellos. */
 
 let num1 = prompt("ingrese el primer número")
@@ -85,7 +85,7 @@ let cua3 = Math.pow(num3, 2);
 
 alert("el cuadrado de los tres numeros es de " +cua1+ " el primer numero," +cua2+" el segundo numero y " +cua3+ " el tercer numero");
 
-/*En un hospital existen tres áreas: Ginecología, Pediatría, Traumatología. El
+/*Ejercicio 7 - En un hospital existen tres áreas: Ginecología, Pediatría, Traumatología. El
 presupuesto anual del hospital se reparte conforme a la sig. tabla:
 Area          : Porcentaje del presupuesto
 ginecología   : 40%
@@ -103,3 +103,24 @@ let rest = parseInt(presupuesto*trau);
 let resp = parseInt(presupuesto*pedi);
 
 alert("el presupuesto del hopital queda dividido de la siguiente manera: $" +resg+ " en el area de ginecología,$" +rest+ " en el area de traumatología y $" +resp+ " en el area de pediatría.");
+
+/*Ejercico 8 - El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%. "REPETIDO"*/
+
+
+/*Ejercicio 9 - Tres personas deciden invertir su dinero para fundar una empresa. Cada una de
+ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte
+con respecto a la cantidad total invertida. */
+
+let pers1 = prompt("Ingrese la cantidad de dinero invertido por la primera persona");
+let pers2 = prompt("Ingrese la cantidad de dinero invertido por la segunda persona");
+let pers3 = prompt("Ingrese la cantidad de dinero invertido por la tercera persona");
+let totalp = parseInt(pers1)+ parseInt(pers2)+ parseInt(pers3);
+let tper1 = parseInt(pers1)* parseInt(100/totalp);
+let tper2 = parseInt(pers2)* parseInt(100/totalp);
+let tper3 = parseInt(pers3)* parseInt(100/totalp);
+
+alert("El porcentaje que cada persona invirtio en la fundacion de la empresa es de " +tper1+ "% la primera persona, " +tper2+ "% la segunda persona y " +tper3+ "% la tercera persona.");
+
+/*Ejercicio 10 - Realizar una calculadora con las operaciones básicas suma, división,
+multiplicación y división. */
